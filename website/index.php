c1e30666f267ba93cae5b4fcd1d69b53282981b7
Jean pierre <d210612@edu.rocwb.nl>
2017-03-13 14:34:45 +0100

    plaatje met wat html/php
    plaatje met wat html/php

---
+++ website/index.php
@@ -0,0 +1,24 @@
+!<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>DeGokkers</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="apple-touch-icon" href="apple-touch-icon.png">
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/normal.min.css">
+  </head>
+  <body>
+    <div class="wrapper">
+      <ul>
+        <li>
+          <p>derp</p>
+        </li>
+      </ul>
+    </div>
+  </body>
+</html>
